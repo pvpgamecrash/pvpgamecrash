c99af1ad9c2b69845b84c6b1db4888f6b9682f30
Talon Bragg <pvpgamecrash@gmail.com>
2016-09-28 18:52:14 -0700

    Update idontknow.js

--- idontknow.js
+++ idontknow.js
@@ -1,8 +1,8 @@
 var myGamePiece;
 
 function startGame() {
-    myGameArea.start();
     myGamePiece = new component(30, 30, "red", 10, 120);
+    myGameArea.start();
 }
 
 var myGameArea = {
@@ -12,6 +12,10 @@ var myGameArea = {
         this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
@@ -20,12 +24,14 @@ function component(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
 }
 
-function updateGameArea(){
-myGameArea.clear();
-myGameArea.update();
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.update();
 }
