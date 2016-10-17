b09679632401a604aae88271ee3626514c05dc2b
Talon Bragg <pvpgamecrash@gmail.com>
2016-10-16 20:50:02 -0700

    Delete Test Javascript
    I deleted the stuff that I had before as a test and I moved it into another file.

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,28 +1,4 @@
-(function(global) {
-	var questions = [{"What is Talon's favorite animal?":"cat"},
-					 {"What does Talon code?":"javascript"}],
-		q = questions.length,
-		qa,
-		question,
-		score = 0;
-	while(q--) {
-		qa = questions[q];
-		for(question in qa) {
-			if(prompt(question)===qa[question]) {
-				confirm('You are right!');
-				score++;
-			}
-			else {
-				confirm("You are wrong, you lose! Better luck next time");
-				document.location.href = 'https://pvpgamecrash.github.io';
-			}
-		}
-		if(score === questions.length) {
-			document.location.href = 'https://pvpgamecrash.github.io/quiz/medal';
-		}
-	}
-})(window)
-//Start back where the var "question_2 is"
+//Main JS for the quiz
 function start() {
 	var question_1 = prompt("What is Talon's favorite animal?");
 	if(question_1 === "cat") {
