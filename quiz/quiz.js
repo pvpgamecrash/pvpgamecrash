286f3801020d56c8bad90cd823cef075e5aa37e2
Talon Bragg <pvpgamecrash@gmail.com>
2016-10-16 20:26:13 -0700

    Update quiz.js

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -3,17 +3,41 @@ function start() {
 	var question_1 = prompt("What is Talon's favorite animal?");
 	if(question_1 === "cat") {
 		confirm("You are right!");
-		var question_2 = prompt("Why does Talon Code")	
-		if (question_2 === "He likes to make things") {
+		var question_2 = prompt("Why does Talon Code?")	
+		if (question_2 === "I like to make things") {
 		 		confirm("You are right again!");
-		 	}
-		 	else {
-		 		alert("You are wrong, you lose! Better luck next time");
-		 		document.location.reload();
+		 		var question_3 = prompt("What job does Talon have?");
+				if (question_3 === "a student") {
+					confirm("You are right once again");	
+						var question_4 = prompt("What school subject is Talon best in?");
+						if (question_4 === "math") {
+						confirm("Yay, you're right; I'm getting tired of you winning");
+							var question_5 = prompt("How old is Talon?");
+							if (question_5 === "12") {
+							confirm("You won the game! Collect your prize by pressing OK.");
+							document.location.href = 'https://pvpgamecrash.github.io/quiz/medal';
+							}
+							else {
+							confirm("You lose! Come back next time!");
+							}
+					}
+					else {
+					confirm("Yes, I am so tired of this game, you lose!");
+					document.location.href = 'https://pvpgamecrash.github.io';
+					}
+				}
+				else {
+				confirm("You are wrong, you lose! Better luck next time");
+		 		document.location.href = 'https://pvpgamecrash.github.io';				
+				}
+			}
+			else {
+		 		confirm("You are wrong, you lose! Better luck next time");
+		 		document.location.href = 'https://pvpgamecrash.github.io';
 		 	}
 		}
 	else {
-		alert("You are wrong, you lose! Better luck next time");
-		document.location.reload();
+		confirm("You are wrong, you lose! Better luck next time");
+		document.location.href = 'https://pvpgamecrash.github.io';
 	}
 }
