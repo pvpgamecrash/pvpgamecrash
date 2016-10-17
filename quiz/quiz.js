89b7c68bcf006b38b310e420000e1e60ced572c8
Talon Bragg <pvpgamecrash@gmail.com>
2016-10-16 20:42:58 -0700

    alternate quiz method

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,3 +1,27 @@
+(function(global) {
+	var questions = [{"What is Talon's favorite animal?":"cat"},
+					 {"What does Talon code?":"javascript"}],
+		q = q.length,
+		qa,
+		question,
+		score = 0;
+	while(q--) {
+		qa = questions[q];
+		for(question in qa) {
+			if(prompt(question)===qa[question]) {
+				confirm('You are right!');
+				score++;
+			}
+			else {
+				confirm("You are wrong, you lose! Better luck next time");
+				document.location.href = 'https://pvpgamecrash.github.io';
+			}
+		}
+		if(score === questions.length) {
+			document.location.href = 'https://pvpgamecrash.github.io/quiz/medal';
+		}
+	}
+})(window)
 //Start back where the var "question_2 is"
 function start() {
 	var question_1 = prompt("What is Talon's favorite animal?");
