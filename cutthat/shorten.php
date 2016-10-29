7be108c7de7872157d59eac18c9d87ffe17d48b8
Talon Bragg <pvpgamecrash@gmail.com>
2016-10-29 12:59:43 -0700

    Update shorten.php

--- cutthat/shorten.php
+++ cutthat/shorten.php
@@ -1,1 +1,16 @@
-
+< ?php  
+  
+$con = mysql_connect("localhost","username","password");  
+if (!$con)  
+  {  
+  die('Could not connect: ' . mysql_error());  
+  }  
+mysql_select_db("DB NAME", $con); //Replace with your MySQL DB Name  
+$urlinput=mysql_real_escape_string($_POST['url']);  
+$id=rand(10000,99999);  
+$shorturl=base_convert($id,20,36);  
+$sql = "insert into TABLE NAME values('$id','$urlinput','$shorturl')";  
+mysql_query($sql,$con);  
+echo "Shortened url is http://cutthat.com/". $shorturl ."";  
+mysql_close($con);  
+?>  
