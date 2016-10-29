1e226b1994a64490767fb265c3607b56d7c17c75
Talon Bragg <pvpgamecrash@gmail.com>
2016-10-29 12:56:33 -0700

    Update index.php

--- cutthat/index.php
+++ cutthat/index.php
@@ -1,1 +1,6 @@
-
+<form id="form1" action="shorten.php" method="post">  
+  
+<strong> Url:</strong>  
+<input id="url" name="url" size="45" type="text">  
+<input id="Submit" name="Submit" type="submit" value="Shorten">  
+</form>  
