b7c649e47829025accc13d09d70294441d5c2dbb
Talon Bragg <pvpgamecrash@gmail.com>
2016-10-29 13:00:39 -0700

    Update decoder.php

--- cutthat/decoder.php
+++ cutthat/decoder.php
@@ -1,1 +1,22 @@
 
+< ?php  
+  
+$con = mysql_connect("localhost","username","password");  
+if (!$con)  
+  {  
+  die('Could not connect: ' . mysql_error());  
+  }  
+mysql_select_db("DB NAME", $con); //Replace with your MySQL DB Name  
+  
+$de= mysql_real_escape_string($_GET["decode"]);  
+  
+$sql = 'select * from TABLE NAME where shortened="$de"';  
+  
+$result=mysql_query("select * from TABLE NAME where shortened='$de'");  
+  
+while($row = mysql_fetch_array($result))  
+{  
+$res=$row['url'];  
+header("location:".$res);  
+}  
+?>  
